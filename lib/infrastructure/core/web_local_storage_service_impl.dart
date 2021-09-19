8130760f41155b482c5188e02f17c41ce909a115
andantonyan <andantonyan@gmail.com>
2021-09-20 00:43:01 +0400

    setup firebase for web, add simple WebLocalStorageServiceImpl

--- lib/infrastructure/core/web_local_storage_service_impl.dart
+++ lib/infrastructure/core/web_local_storage_service_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_archetype/domain/domain.dart';
 import 'package:injectable/injectable.dart';
 
@@ -6,6 +8,37 @@ import 'di.env.dart';
 @web
 @LazySingleton(as: StorageService)
 class WebLocalStorageServiceImpl implements StorageService {
+  static const _defaultLocale = Locale('en');
+
+  String? _token;
+  Locale? _locale;
+
   @override
-  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+  Future<void> deleteAuthToken() async {
+    _token = null;
+  }
+
+  @override
+  Future<String?> getAuthToken() async {
+    return _token;
+  }
+
+  @override
+  Future<Locale> getSelectedLocale() async {
+    return _locale ?? _defaultLocale;
+  }
+
+  @override
+  Future<void> init() async {
+  }
+
+  @override
+  Future<void> saveAuthToken(String token) async {
+    _token = token;
+  }
+
+  @override
+  Future<void> saveSelectedLocale(Locale locale) async {
+    _locale = locale;
+  }
 }
